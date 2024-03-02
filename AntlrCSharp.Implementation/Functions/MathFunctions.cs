ca814a53595fe162592991684a2def5660de852b
Harry Cordewener <admin@twilightdays.org>
2024-03-01 23:58:47 -0600

    Do math with aggregation and a helper function for validation.

--- AntlrCSharp.Implementation/Functions/MathFunctions.cs
+++ AntlrCSharp.Implementation/Functions/MathFunctions.cs
@@ -4,8 +4,7 @@ namespace AntlrCSharp.Implementation.Functions
 {
 	public static partial class Functions
 	{
-		[PennFunction(Name = "add", Flags = FunctionFlags.Regular | FunctionFlags.StripAnsi | FunctionFlags.DecimalsOnly)]
-		public static CallState Add(Parser _1, PennFunctionAttribute _2, params CallState[] args)
+		private static CallState ValidateAndEvaluate(CallState[] args, Func<decimal, decimal, decimal> aggregateFunction)
 		{
 			var doubles = args.Select(x =>
 				(
@@ -15,7 +14,23 @@ namespace AntlrCSharp.Implementation.Functions
 
 			return doubles.Any(x => !x.IsDouble)
 					? new CallState(Message: Errors.ErrorNumbers)
-					: new CallState(Message: doubles.Sum(x => x.Double).ToString());
+					: new CallState(Message: doubles.Select(x => x.Double).Aggregate(aggregateFunction).ToString());
 		}
+
+		[PennFunction(Name = "add", Flags = FunctionFlags.Regular | FunctionFlags.StripAnsi | FunctionFlags.DecimalsOnly)]
+		public static CallState Add(Parser _1, PennFunctionAttribute _2, params CallState[] args) =>
+			ValidateAndEvaluate(args, (acc, sub) => acc + sub);
+
+		[PennFunction(Name = "sub", Flags = FunctionFlags.Regular | FunctionFlags.StripAnsi | FunctionFlags.DecimalsOnly)]
+		public static CallState Sub(Parser _1, PennFunctionAttribute _2, params CallState[] args) =>
+			ValidateAndEvaluate(args, (acc, sub) => acc - sub);
+
+		[PennFunction(Name = "mul", Flags = FunctionFlags.Regular | FunctionFlags.StripAnsi | FunctionFlags.DecimalsOnly)]
+		public static CallState Mul(Parser _1, PennFunctionAttribute _2, params CallState[] args) =>
+			ValidateAndEvaluate(args, (acc, sub) => acc * sub);
+
+		[PennFunction(Name = "div", Flags = FunctionFlags.Regular | FunctionFlags.StripAnsi | FunctionFlags.DecimalsOnly)]
+		public static CallState Div(Parser _1, PennFunctionAttribute _2, params CallState[] args) =>
+			ValidateAndEvaluate(args, (acc, sub) => acc / sub);
 	}
 }
