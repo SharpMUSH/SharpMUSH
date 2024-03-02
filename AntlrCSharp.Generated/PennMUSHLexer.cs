d2ab99a6173ca9fd3beba36d16d72720c0386cdf
Harry Cordewener <admin@twilightdays.org>
2024-03-02 00:39:24 -0600

    Less footprint please.

--- AntlrCSharp.Generated/PennMUSHLexer.cs
+++ AntlrCSharp.Generated/PennMUSHLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from c:/Users/admin/OneDrive/Documents/Repos/MUParser/AntlrCSharp.Generated/PennMUSHLexer.g4 by ANTLR 4.13.1
+// Generated from d:/CSharpPennMUSH/AntlrCSharp.Generated/PennMUSHLexer.g4 by ANTLR 4.13.1
 
 // Unreachable code detected
 #pragma warning disable 0162
--- AntlrCSharp.Generated/PennMUSHParserBaseVisitor.cs
+++ AntlrCSharp.Generated/PennMUSHParserBaseVisitor.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from c:/Users/admin/OneDrive/Documents/Repos/MUParser/AntlrCSharp.Generated/PennMUSHParser.g4 by ANTLR 4.13.1
+// Generated from d:/CSharpPennMUSH/AntlrCSharp.Generated/PennMUSHParser.g4 by ANTLR 4.13.1
 
 // Unreachable code detected
 #pragma warning disable 0162
