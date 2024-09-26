fac981b5b18336b1b8d02fa5b1bcd54c926a8620
Harry Cordewener <Admin@twilightdays.org>
2024-09-26 12:30:34 -0500

    What the Heck?

--- SharpMUSH.Tests/Functions/IterationFunctionUnitTests.cs
+++ SharpMUSH.Tests/Functions/IterationFunctionUnitTests.cs
@@ -4,6 +4,7 @@ public class IterationFunctionUnitTests : BaseUnitTest
 {
 	[Test]
 	[Arguments("iter(1 2 3,%i0)", "1 2 3")]
+	[Arguments("iter(1,%i1)", "#-1 OUT OF RANGE")]
 	[Arguments("iter(1 2 3,add(%i0,1))", "2 3 4")]
 	[Arguments("iter(1|2|3,%i0,|)", "1|2|3")]
 	[Arguments("iter(1|2|3,%i0,|,-)", "1-2-3")]
@@ -32,6 +33,7 @@ public class IterationFunctionUnitTests : BaseUnitTest
 	[Arguments("iter(1|2|3,iter(1 2 3,[add(%i0,%i1)][ibreak()]),|,-)", "2 3 4")]
 	[Arguments("iter(1|2|3,iter(1 2 3,[add(%i0,%i1)][ibreak(0)]),|,-)", "2 3 4")]
 	[Arguments("iter(1|2|3,iter(1 2 3,[add(%i0,%i1)][ibreak(1)]),|,-)", "2-3-4")]
+	// [Arguments("iter(1|2|3,iter(1 2 3,[ibreak(1)][add(%i0,%i1)]),|,-)", "2-3-4")]
 	// TODO: Why does putting [ibreak()] at the start of the contents cause a different evaluation?
 	public async Task IterationBreak(string function, string expected)
 	{
