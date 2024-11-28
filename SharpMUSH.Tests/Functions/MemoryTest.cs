3bea19d5340d69950117a524d26991bf1d23f3a3
HarryCordewener <admin@twilightdays.org>
2024-11-27 20:09:21 -0600

    Prep for simple debug output.

--- SharpMUSH.Tests/Functions/MemoryTest.cs
+++ SharpMUSH.Tests/Functions/MemoryTest.cs
@@ -4,27 +4,49 @@ namespace SharpMUSH.Tests.Functions;
 
 public class MemoryTest : BaseUnitTest
 {
-		[Test]
-		public async Task Depth()
+	[Test]
+	public async Task Depth()
+	{
+		var sb = new StringBuilder();
+		foreach (var _ in Enumerable.Range(0, 200))
 		{
-				var sb = new StringBuilder();
-				foreach (var _ in Enumerable.Range(0, 200))
-				{
-						sb.Append("add(1,");
-				}
-				sb.Append('1');
-				foreach (var _ in Enumerable.Range(0, 200))
-				{
-						sb.Append(')');
-				}
-				var str = sb.ToString();
-
-				var parser = await TestParser();
-				var result = (await parser.FunctionParse(MModule.single(str)))?.Message?.ToString();
-
-				await Assert
-					.That(result)
-					.IsEqualTo("201");
+			sb.Append("add(1,");
 		}
+		sb.Append('1');
+		foreach (var _ in Enumerable.Range(0, 200))
+		{
+			sb.Append(')');
+		}
+		var str = sb.ToString();
+
+		var parser = await TestParser();
+		var result = (await parser.FunctionParse(MModule.single(str)))?.Message?.ToString();
+
+		await Assert
+			.That(result)
+			.IsEqualTo("201");
+	}
+	[Test]
+	public async Task SmallDepth()
+	{
+		var sb = new StringBuilder();
+		foreach (var _ in Enumerable.Range(0, 10))
+		{
+			sb.Append("add(1,");
+		}
+		sb.Append('1');
+		foreach (var _ in Enumerable.Range(0, 10))
+		{
+			sb.Append(')');
+		}
+		var str = sb.ToString();
+
+		var parser = await TestParser();
+		var result = (await parser.FunctionParse(MModule.single(str)))?.Message?.ToString();
+
+		await Assert
+			.That(result)
+			.IsEqualTo("11");
+	}
 }
 
