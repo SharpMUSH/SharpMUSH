68f7168e06249e2e93cdf0d1e311b7037411dfb1
HarryCordewener <admin@twilightdays.org>
2024-12-16 16:30:51 -0600

    Even more explicit.

--- SharpMUSH.Implementation/Visitors/SharpMUSHParserVisitor.cs
+++ SharpMUSH.Implementation/Visitors/SharpMUSHParserVisitor.cs
@@ -20,21 +20,28 @@ public class SharpMUSHParserVisitor(IMUSHCodeParser parser, MString source)
 	protected override async ValueTask<CallState?> AggregateResult(ValueTask<CallState?> aggregate, ValueTask<CallState?> nextResult)
 		=> (await aggregate, await nextResult) switch
 		{
-			({ Arguments: not null } agg, 
-			 { Arguments: not null } next)
+			(null,null) => null,
+			({ Arguments: not null } agg,
+			{ Arguments: not null } next)
 				=> agg with { Arguments = [.. agg.Arguments, .. next.Arguments] },
-			({ Arguments: not null } agg, 
-			 { Arguments: null })
+			({ Arguments: not null } agg,
+			{ Arguments: null })
 				=> agg,
-			({ Arguments: null }, 
-			 { Arguments: not null } next)
+			({ Arguments: null },
+			{ Arguments: not null } next)
 				=> next,
-			({ Message: not null } agg, 
-			 { Message: not null } next)
+			({ Message: not null } agg,
+			{ Message: not null } next)
 				=> agg with { Message = MModule.concat(agg.Message, next.Message) },
+			({ Message: not null } agg,
+			{ Message: null })
+				=> agg,
+			({ Message: null },
+			{ Message: not null } next)
+				=> next,
 			(var agg, null) => agg,
 			(null, var next) => next,
-			(var agg, _) => agg
+			_ => throw new InvalidOperationException("Invalid state")
 		};
 
 	public override async ValueTask<CallState?> VisitFunction([NotNull] FunctionContext context)
