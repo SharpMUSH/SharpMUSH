56d24c336fa1cc898bb548a0df3daddd3b439452
Harry Cordewener <admin@twilightdays.org>
2024-12-27 17:34:35 -0600

    Helpfile tests, not yet fully functioning.

---
+++ SharpMUSH.Documentation/Helpfiles.cs
@@ -0,0 +1,48 @@
+using System.Text.RegularExpressions;
+using OneOf;
+using OneOf.Types;
+
+namespace SharpMUSH.Documentation;
+
+public partial class Helpfiles(DirectoryInfo directory)
+{
+	public void Index()
+	{
+		var files = directory.GetFiles("*.hlp");
+		foreach (var file in files)
+		{
+			Index(file);	
+		}
+	}
+
+	public static OneOf<Dictionary<string,string>, Error<string>> Index(FileInfo file)
+	{
+		if (!file.Exists)
+		{
+			return new Error<string>($"File {file.FullName} does not exist.");
+		}
+
+		var dict = new Dictionary<string, string>();
+
+		using var openText = file.OpenText();
+		var textBody = openText.ReadToEnd();
+		
+		var matches = Indexes().Matches(textBody);
+
+		foreach (Match match in matches)
+		{
+			var indexes = match.Groups["Indexes"].Captures.Select(x => x.Value);
+			var body = match.Groups["Body"].Value;
+
+			foreach (var index in indexes)
+			{
+				dict.Add(index, body);
+			}
+		}
+
+		return dict;
+	}
+
+	[GeneratedRegex(@"(?:^& (?<Indexes>.+)\\r\\n)+(?<Body>.+)", RegexOptions.Compiled | RegexOptions.Multiline)]
+	private static partial Regex Indexes();
+}
---
+++ SharpMUSH.Tests/Documentation/HelpfileTests.cs
@@ -0,0 +1,19 @@
+﻿namespace SharpMUSH.Tests.Documentation;
+
+public class HelpfileTests
+{
+	[Test]
+	public async Task Indexable()
+	{
+		var currentDirectory = Directory.GetCurrentDirectory();
+		var fileString = Path.Combine(currentDirectory, "..", "..", "..", "..", "SharpMUSH.Documentation", "Helpfiles", "PennMUSH", "pennattr.hlp");
+		var fileInfo = new FileInfo(fileString);
+		var maybeIndexes = SharpMUSH.Documentation.Helpfiles.Index(fileInfo);
+
+		await Assert.That(maybeIndexes.IsT1).IsNotEqualTo(true);
+
+		var indexes = maybeIndexes.AsT0;
+
+		// await Assert.That(indexes).IsNotEmpty();
+	}
+}
