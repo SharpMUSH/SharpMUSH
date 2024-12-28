1993ca60c720b0a9332d4a2a4b33a6e4ec8a90d5
Harry Cordewener <admin@twilightdays.org>
2024-12-27 20:51:51 -0600

    Git issues

--- SharpMUSH.Documentation/Helpfiles.cs
+++ SharpMUSH.Documentation/Helpfiles.cs
@@ -27,17 +27,20 @@ public partial class Helpfiles(DirectoryInfo directory)
 		using var openText = file.OpenText();
 		
 		var textBody = openText.ReadToEnd();
-		Console.WriteLine($"TEXTBODY: {textBody}");
-		
 		var matches = Indexes().Matches(textBody);
 
+		Console.WriteLine($"STARTING INDEX MATCHING FOR {matches.Count} MATCHES");
 		foreach (Match match in matches)
 		{
+			Console.WriteLine("INDEX MATCH");
 			var indexes = match.Groups["Indexes"].Captures.Select(x => x.Value);
 			var body = match.Groups["Body"].Value;
+			Console.WriteLine($"INDEX MATCH INDEXES: {string.Join(", ", indexes)}");
+			Console.WriteLine($"INDEX MATCH BODY: {body}");
 
 			foreach (var index in indexes)
 			{
+				Console.WriteLine($"INDEX MATCH ADDING: {index}");
 				dict.Add(index, body);
 			}
 		}
