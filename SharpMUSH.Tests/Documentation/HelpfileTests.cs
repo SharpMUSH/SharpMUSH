009c2e213d93fc93fd7b1520b8c87b0a163bf875
Harry Cordewener <admin@twilightdays.org>
2024-12-27 21:13:06 -0600

    More Tests around Help File Indexing.

--- SharpMUSH.Tests/Documentation/HelpfileTests.cs
+++ SharpMUSH.Tests/Documentation/HelpfileTests.cs
@@ -3,12 +3,14 @@
 public class HelpfileTests
 {
 	[Test]
-	public async Task Indexable()
+	[Arguments("pennattr.hlp", new[] { "ATTRIBUTE TREES", "ATTR TREES", "ATTRIB TREES", "`" })]
+	[Arguments("pennchat.hlp", new[] { "@CEMIT", "@NSCEMIT", "CEMIT()", "NSCEMIT()" })]
+	public async Task Indexable(string file, string[] aliasTest)
 	{
 		var currentDirectory = Directory.GetCurrentDirectory();
-		var fileString = Path.Combine(currentDirectory, "Documentation", "Testfile", "pennattr.hlp");
+		var fileString = Path.Combine(currentDirectory, "Documentation", "Testfile", file);
 		var fileInfo = new FileInfo(fileString);
-		
+
 		var maybeIndexes = SharpMUSH.Documentation.Helpfiles.Index(fileInfo);
 
 		await Assert.That(maybeIndexes.IsT1).IsNotEqualTo(true);
@@ -17,14 +19,14 @@ public class HelpfileTests
 
 		await Assert.That(indexes).IsNotEmpty();
 
-		foreach (var key in new[] { "ATTRIBUTE TREES", "ATTR TREES", "ATTRIB TREES", "`" })
+		foreach (var key in aliasTest)
 		{
 			await Assert.That(indexes).ContainsKey(key);
 		}
-		
-		foreach (var key in new[] { "ATTRIBUTE TREES", "ATTR TREES", "ATTRIB TREES" })
+
+		foreach (var key in aliasTest.Skip(1))
 		{
-			await Assert.That(indexes[key]).IsEqualTo(indexes["`"]);
+			await Assert.That(indexes[key]).IsEqualTo(indexes[aliasTest.First()]);
 		}
 	}
 }
