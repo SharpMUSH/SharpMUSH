795f94cd00a05b8301b05e0d17256c4cb55e8d40
Harry Cordewener <admin@twilightdays.org>
2025-01-09 00:12:45 -0600

    WIP on Objects for Issue #90 and Issue #91 for @channels and @mail

---
+++ SharpMUSH.Library/Models/SharpChannel.cs
@@ -0,0 +1,24 @@
+﻿namespace SharpMUSH.Library.Models;
+
+public class SharpChannel
+{
+	public required string Name { get; set; }
+	public string Description { get; set; } = string.Empty;	
+	public required Lazy<SharpPlayer> Owner { get; set; }
+	public required Func<IEnumerable<SharpObject>> Members { get; set; }
+	public required string[] Privs { get; set; }
+	public string JoinLock { get; set; } = string.Empty;
+	public string SpeakLock { get; set; } = string.Empty;
+	public string SeeLock { get; set; } = string.Empty;
+	public string HideLock { get; set; } = string.Empty;
+	public string ModLock { get; set; } = string.Empty;
+}
+
+public class ChannelStatus
+{
+	public required bool Gagged { get; set; } = false;
+	public required bool Mute { get; set; } = false;
+	public required bool Hide { get; set; } = false;
+	public required bool Combine { get; set; } = false;
+	public required string Title { get; set; } = string.Empty;
+}
---
+++ SharpMUSH.Library/Models/SharpMail.cs
@@ -0,0 +1,15 @@
+﻿namespace SharpMUSH.Library.Models;
+
+public class SharpMail
+{
+	public required DateTimeOffset DateSent { get; set; }
+	public required bool Fresh { get; set; }
+	public required bool Read { get; set; }
+	public required bool Tagged { get; set; }
+	public required bool Urgent { get; set; }
+	public required bool Cleared { get; set; }
+	public required string Folder { get; set; }
+	public required string Content { get; set; }
+	public required string Subject { get; set; }
+	public required Lazy<SharpObject> From { get; set; }
+}
