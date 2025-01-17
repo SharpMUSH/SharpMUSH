51af9a1adc95e37a3cfe6d3efc0ce7fb63642cc7
Harry Cordewener <admin@twilightdays.org>
2025-01-16 23:32:56 -0600

    CStats Setup.

--- SharpMUSH.Implementation/Commands/MailCommand/StatsMail.cs
+++ SharpMUSH.Implementation/Commands/MailCommand/StatsMail.cs
@@ -6,6 +6,27 @@ public static class StatsMail
 {
 	public static async ValueTask<MString>  Handle(IMUSHCodeParser parser, MString? arg0, MString? arg1, string[] switches)
 	{
+		/*
+		 * STATS: 
+		Liminality sent 59 messages.
+		Liminality has 145 messages.
+
+		* DSTATS:
+		Mail statistics for Liminality:
+		59 messages sent, 12 unread, 0 cleared.
+		145 messages received, 102 unread, 0 cleared.
+		Last is dated Mon Jul 11 02:47:26 2022
+
+		* FSTATS:
+		Mail statistics for Liminality:
+		59 messages sent, 12 unread, 0 cleared, totalling 10000 characters.
+		145 messages received, 102 unread, 0 cleared, totalling 24638 characters.
+		Last is dated Mon Jul 11 02:47:26 2022
+		
+		* CSTATS:
+		MAIL: 145 messages in folder 0 [INBOX] (102 unread, 0 cleared).
+		*/
+		
 		await ValueTask.CompletedTask;
 		throw new NotImplementedException();
 	}
