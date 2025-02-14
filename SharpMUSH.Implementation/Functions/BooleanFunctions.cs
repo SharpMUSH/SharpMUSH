7037f794b329ad4be37fc3c7329ef06d8cb14a8e
Harry Cordewener <admin@twilightdays.org>
2025-02-14 10:18:34 -0600

    Ensure more async instead of GetResult calls.

--- SharpMUSH.Implementation/Functions/BooleanFunctions.cs
+++ SharpMUSH.Implementation/Functions/BooleanFunctions.cs
@@ -13,20 +13,22 @@ public static partial class Functions
 				: "0"));
 
 	[SharpFunction(Name = "cand", Flags = FunctionFlags.Regular | FunctionFlags.NoParse)]
-	public static ValueTask<CallState> Cand(IMUSHCodeParser parser, SharpFunctionAttribute _2)
-		=> ValueTask.FromResult<CallState>(new(parser.CurrentState.Arguments
+	public static async ValueTask<CallState> Cand(IMUSHCodeParser parser, SharpFunctionAttribute _2)
+		=> new(await parser.CurrentState.Arguments
 			.Select(x => x.Value.Message!)
-			.All(m => parser.FunctionParse(m).AsTask().Result!.Message.Truthy())
+			.ToAsyncEnumerable()
+			.AllAwaitAsync(async m => (await parser.FunctionParse(m))!.Message.Truthy())
 			? "0"
-			: "1"));
+			: "1");
 
 	[SharpFunction(Name = "cor", Flags = FunctionFlags.Regular | FunctionFlags.NoParse)]
-	public static ValueTask<CallState> Cor(IMUSHCodeParser parser, SharpFunctionAttribute _2)
-		=> ValueTask.FromResult<CallState>(new(parser.CurrentState.Arguments
+	public static async ValueTask<CallState> Cor(IMUSHCodeParser parser, SharpFunctionAttribute _2)
+		=> new(await parser.CurrentState.Arguments
 			.Select(x => x.Value.Message!)
-			.Any(m => parser.FunctionParse(m).AsTask().Result!.Message.Truthy())
+			.ToAsyncEnumerable()
+			.AllAwaitAsync(async m => (await parser.FunctionParse(m))!.Message.Truthy())
 			? "1"
-			: "0"));
+			: "0");
 
 	[SharpFunction(Name = "eq", Flags = FunctionFlags.Regular | FunctionFlags.DecimalsOnly)]
 	public static ValueTask<CallState> Eq(IMUSHCodeParser parser, SharpFunctionAttribute _2)
@@ -88,11 +90,13 @@ public static partial class Functions
 			new(parser.CurrentState.Arguments.Select(x => x.Value.Message!).All(Predicates.Falsey) ? "1" : "0"));
 
 	[SharpFunction(Name = "ncor", Flags = FunctionFlags.Regular | FunctionFlags.NoParse)]
-	public static ValueTask<CallState> NCor(IMUSHCodeParser parser, SharpFunctionAttribute _2)
-		=> ValueTask.FromResult<CallState>(new(parser.CurrentState.Arguments.Select(x => x.Value.Message!)
-			.All(m => parser.FunctionParse(m).AsTask().Result!.Message.Falsey())
+	public static async ValueTask<CallState> NCor(IMUSHCodeParser parser, SharpFunctionAttribute _2)
+		=> new(await parser.CurrentState.Arguments
+			.Select(x => x.Value.Message!)
+			.ToAsyncEnumerable()
+			.AllAwaitAsync(async m => (await parser.FunctionParse(m))!.Message.Falsey())
 			? "1"
-			: "0"));
+			: "0");
 
 	[SharpFunction(Name = "not", Flags = FunctionFlags.Regular, MinArgs = 1, MaxArgs = 1)]
 	public static ValueTask<CallState> Not(IMUSHCodeParser parser, SharpFunctionAttribute _2)
