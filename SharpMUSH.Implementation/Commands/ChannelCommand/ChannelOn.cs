1d1c8b2760e83e0bd62afea956c5e7b93ade937d
Harry Cordewener <Admin@twilightdays.org>
2025-02-26 13:31:09 -0600

    ChannelJoin

--- SharpMUSH.Implementation/Commands/ChannelCommand/ChannelOn.cs
+++ SharpMUSH.Implementation/Commands/ChannelCommand/ChannelOn.cs
@@ -1,6 +1,8 @@
+using SharpMUSH.Library.Commands.Database;
 using SharpMUSH.Library.Extensions;
 using SharpMUSH.Library.ParserInterfaces;
 using SharpMUSH.Library.Queries.Database;
+using SharpMUSH.Library.Services;
 
 namespace SharpMUSH.Implementation.Commands.ChannelCommand;
 
@@ -10,8 +12,17 @@ public static class ChannelOn
 	{
 		var executor = (await parser.CurrentState.ExecutorObject(parser.Mediator)).Known();
 		var targetName = arg1.ToPlainText();
-		// TODO: Use Locate
-		var target = await parser.Mediator.Send(new GetPlayerQuery(targetName));
+
+		var maybeTarget = await parser.LocateService.Locate(parser, executor, executor, targetName, LocateFlags.PlayersPreference | LocateFlags.OnlyMatchTypePreference | LocateFlags.MatchOptionalWildCardForPlayerName);
+
+		if (maybeTarget.IsError)
+		{
+			await parser.NotifyService.Notify(executor, maybeTarget.AsError.Value);
+			return new CallState(maybeTarget.AsError.Value);
+		}
+
+		var target = maybeTarget.AsAnyObject;
+		
 		var maybeChannel = await ChannelHelper.GetChannelOrError(parser, channelName, true);
 
 		if (maybeChannel.IsError)
@@ -21,15 +32,10 @@ public static class ChannelOn
 
 		var channel = maybeChannel.AsChannel;
 
-		if (!target.Any())
-		{
-			await parser.NotifyService.Notify(executor, $"#-1 Player {targetName} not found.");
-			return new CallState("#-1 Player not found.");
-		}
-
 		// TODO: Announce Channel Join
-		// await parser.Mediator.Send(new JoinChannelCommand(target, channel));
-		await parser.NotifyService.Notify(executor, $"#-1 {targetName} has been added to {channelName}.");
-		return new CallState($"#-1 {targetName} has been added to {channelName}.");
+		await parser.Mediator.Send(new AddUserToChannelCommand(channel, target));
+
+		await parser.NotifyService.Notify(executor, $"CHAT: {targetName} has been added to {channelName}.");
+		return new CallState($"{targetName} has been added to {channelName}.");
 	}
 }
