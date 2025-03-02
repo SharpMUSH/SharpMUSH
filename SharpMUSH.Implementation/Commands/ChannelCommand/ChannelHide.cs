b79e298dd02307d6fefaad3d66fa771c5438727a
Harry Cordewener <Admin@twilightdays.org>
2025-03-02 14:13:46 -0600

    ChannelHide work.

--- SharpMUSH.Implementation/Commands/ChannelCommand/ChannelHide.cs
+++ SharpMUSH.Implementation/Commands/ChannelCommand/ChannelHide.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using SharpMUSH.Library.Commands.Database;
 using SharpMUSH.Library.Extensions;
 using SharpMUSH.Library.Models;
@@ -11,11 +12,19 @@ public static class ChannelHide
 	public static async ValueTask<CallState> Handle(IMUSHCodeParser parser, MString? channelName, MString? yesNo)
 	{
 		var executor = (await parser.CurrentState.ExecutorObject(parser.Mediator)).Known();
-		IEnumerable<SharpChannel> channels;
+		ImmutableArray<SharpChannel> channels;
+
+		var yesNoString = yesNo?.ToPlainText();
+		if (yesNoString is not null && !(yesNoString.Equals("yes", StringComparison.InvariantCultureIgnoreCase) ||
+		                                 yesNoString.Equals("no", StringComparison.InvariantCultureIgnoreCase)))
+		{
+			await parser.NotifyService.Notify(executor, "CHAT: Yes or No are the only valid options.");
+			return new CallState("#-1 INVALID OPTION");
+		}
 
 		if (channelName != null)
 		{
-			channels = await parser.Mediator.Send(new GetChannelListQuery());
+			channels = [..await parser.Mediator.Send(new GetChannelListQuery())];
 		}
 		else
 		{
@@ -27,6 +36,8 @@ public static class ChannelHide
 
 			channels = [maybeChannel.AsChannel];
 		}
+		
+		var hideOn = yesNoString?.Equals("yes", StringComparison.OrdinalIgnoreCase) ?? true;
 
 		foreach (var channel in channels)
 		{
@@ -40,17 +51,24 @@ public static class ChannelHide
 			var member = maybeMemberStatus.Value.Member;
 			var status = maybeMemberStatus.Value.Status;
 
-			if (status.Hide ?? false)
+			if (status.Hide ?? false == hideOn)
 			{
-				return new CallState($"CHAT: You are already hidden on {channel.Name.ToPlainText()}.");
+				return new CallState($"CHAT: You are already in that hide state on {channel.Name.ToPlainText()}.");
 			}
 
 			await parser.Mediator.Send(new UpdateChannelUserStatusCommand(
-				channel, member, status with { Hide = true }));
+				channel, member, status with { Hide = hideOn }));
 
-			return new CallState($"CHAT: You have been hidden on {channel.Name.ToPlainText()}.");
+			if (hideOn)
+			{
+				await parser.NotifyService.Notify(executor, $"CHAT: You have been hidden on {channel.Name.ToPlainText()}.");
+			}
+			else
+			{
+				await parser.NotifyService.Notify(executor, $"CHAT: You have been unhidden on {channel.Name.ToPlainText()}.");
+			}
 		}
 
-		return new CallState("");
+		return new CallState(channels.Length);
 	}
 }
