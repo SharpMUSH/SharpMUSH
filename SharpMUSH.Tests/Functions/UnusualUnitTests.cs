2700c8caa876dc8c582cd45facb136081d66810b
Harry Cordewener <admin@twilightdays.org>
2025-03-21 16:04:18 -0500

    Start enabling evaluation in function names.

---
+++ SharpMUSH.Tests/Functions/UnusualUnitTests.cs
@@ -0,0 +1,25 @@
+using NSubstitute;
+using SharpMUSH.Library.ParserInterfaces;
+using SharpMUSH.Library.Services;
+
+namespace SharpMUSH.Tests.Functions;
+
+public class UnusualUnitTests : BaseUnitTest
+{
+	private static readonly IMUSHCodeParser Parser = TestParser(ns: Substitute.For<INotifyService>())
+		.ConfigureAwait(false)
+		.GetAwaiter()
+		.GetResult();
+
+	[Test]
+	[Arguments(@"s(ansi\(rG\,ansi(D\,[ansi(y,foo)]\)\))", @"foo")]
+	public async Task S(string str, string expected)
+	{
+		Console.WriteLine("Testing: {0}", str);
+
+		var result = (await Parser.FunctionParse(MModule.single(str)))?.Message?.ToString();
+
+		Console.WriteLine("Result: {0}", result);
+		await Assert.That(result).IsEqualTo(expected);
+	}
+}
