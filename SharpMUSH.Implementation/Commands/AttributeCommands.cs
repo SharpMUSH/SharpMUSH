5c4454bca4dbb2b5632853a45ce0ded5e77e4059
HarryCordewener <admin@twilightdays.org>
2025-04-02 15:52:26 -0500

    ATTRIB_SET is not needed.

--- SharpMUSH.Implementation/Commands/AttributeCommands.cs
+++ SharpMUSH.Implementation/Commands/AttributeCommands.cs
@@ -1,16 +1,5 @@
-﻿using SharpMUSH.Library.DiscriminatedUnions;
-using SharpMUSH.Library.ParserInterfaces;
-using CB = SharpMUSH.Implementation.Definitions.CommandBehavior;
-
-namespace SharpMUSH.Implementation.Commands;
+﻿namespace SharpMUSH.Implementation.Commands;
 
 public static partial class Commands
 {
-	[SharpCommand(Name = "ATTRIB_SET", Switches = [], Behavior = CB.Default | CB.EqSplit | CB.NoGagged | CB.Internal,
-		MinArgs = 0, MaxArgs = 0)]
-	public static async ValueTask<Option<CallState>> ATTRIB_SET(IMUSHCodeParser parser, SharpCommandAttribute _2)
-	{
-		await ValueTask.CompletedTask;
-		throw new NotImplementedException();
-	}
 }
