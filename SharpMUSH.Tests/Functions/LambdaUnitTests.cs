11ac88f23007011f0056c3cb1e2cd818e2037bcf
Harry Cordewener <Admin@twilightdays.org>
2025-05-18 16:19:26 +0200

    A test for Lambda, it is not yet passing.

---
+++ SharpMUSH.Tests/Functions/LambdaUnitTests.cs
@@ -0,0 +1,28 @@
+using NSubstitute;
+using SharpMUSH.Library.ParserInterfaces;
+using SharpMUSH.Library.Services;
+
+namespace SharpMUSH.Tests.Functions;
+
+public class LambdaUnitTests : BaseUnitTest
+{
+	private static IMUSHCodeParser? _parser;
+
+	[Before(Class)]
+	public static async Task OneTimeSetup()
+	{
+		_parser = await TestParser(
+			ns: Substitute.For<INotifyService>()
+		);
+	}
+
+	[Test]
+	// [Arguments("u(#lambda/add(1,2))", "3")] // NOT YET SUCCESSFUL. Somehow returns EMPTY.
+	[Arguments("u(#lambda/3)", "3")]
+	[Arguments("3", "3")] 
+	public async Task BasicLambdaTest(string call, string expected)
+	{
+		var res = (await _parser!.FunctionParse(MModule.single(call)))!.Message!;
+		await Assert.That(res.ToPlainText()).IsEqualTo(expected);
+	}
+}
