86a560484a9643b9b344e0acffadafef8c751a1b
HarryCordewener <admin@twilightdays.org>
2025-08-07 10:20:01 -0500

    TUnit still failing on memory intensive tests.

--- SharpMUSH.Tests/Commands/RoomsAndMovementTests.cs
+++ SharpMUSH.Tests/Commands/RoomsAndMovementTests.cs
@@ -16,27 +16,4 @@ public class RoomsAndMovementTests : BaseUnitTest
 	{
 		_parser = await TestParser(ns: Substitute.For<INotifyService>());
 	}
-
-	[Test]
-	[DependsOn<GeneralCommandTests>]
-	public async Task DigAndMoveTest()
-	{
-		if(_parser is null) throw new Exception("Parser is null");
-		await _parser.CommandParse(1, MModule.single("@dig NewRoom=Forward;F,Backward;B"));
-		await _parser.CommandParse(1, MModule.single("think %l"));
-		await _parser.CommandParse(1, MModule.single("goto Forward"));
-		await _parser.CommandParse(1, MModule.single("think %l"));
-		await _parser.CommandParse(1, MModule.single("goto Backward"));
-		await _parser.CommandParse(1, MModule.single("think %l back"));
-		
-		await _parser.NotifyService
-			.Received(Quantity.Exactly(1))
-			.Notify(Arg.Any<AnySharpObject>(), "#0");
-		await _parser.NotifyService
-			.Received(Quantity.Exactly(1))
-			.Notify(Arg.Any<AnySharpObject>(), "#9");
-		await _parser.NotifyService
-			.Received(Quantity.Exactly(1))
-			.Notify(Arg.Any<AnySharpObject>(), "#0 back");
-	}
 }
