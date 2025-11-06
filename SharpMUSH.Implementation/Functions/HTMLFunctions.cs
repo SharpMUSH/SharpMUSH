84d18b874406fc4f2f2fb7bc871680cd7a71ea5b
HarryCordewener <admin@twilightdays.org>
2025-11-06 12:16:27 -0600

    WIP

--- SharpMUSH.Implementation/Functions/HTMLFunctions.cs
+++ SharpMUSH.Implementation/Functions/HTMLFunctions.cs
@@ -2,9 +2,7 @@
 using SharpMUSH.Library.Definitions;
 using SharpMUSH.Library.DiscriminatedUnions;
 using SharpMUSH.Library.ParserInterfaces;
-using SharpMUSH.Library.Services.Interfaces;
 using static SharpMUSH.Library.Services.Interfaces.LocateFlags;
-using static MarkupString.MarkupStringModule;
 using static MarkupString.MarkupImplementation;
 
 namespace SharpMUSH.Implementation.Functions;
@@ -12,7 +10,7 @@ namespace SharpMUSH.Implementation.Functions;
 public partial class Functions
 {
 	[SharpFunction(Name = "html", MinArgs = 1, MaxArgs = 1, Flags = FunctionFlags.Regular | FunctionFlags.WizardOnly)]
-	public static ValueTask<CallState> html(IMUSHCodeParser parser, SharpFunctionAttribute _2)
+	public static ValueTask<CallState> HTML(IMUSHCodeParser parser, SharpFunctionAttribute _2)
 	{
 		// TODO: This probably needs to be more complex than that.
 		return new ValueTask<CallState>(new CallState(
@@ -24,29 +22,27 @@ public partial class Functions
 	}
 
 	[SharpFunction(Name = "tag", MinArgs = 1, MaxArgs = int.MaxValue, Flags = FunctionFlags.Regular)]
-	public static ValueTask<CallState> tag(IMUSHCodeParser parser, SharpFunctionAttribute _2)
-	{
-		throw new NotImplementedException();
-	}
+	public static ValueTask<CallState> Tag(IMUSHCodeParser parser, SharpFunctionAttribute _2)
+		=> ValueTask.FromResult<CallState>("#-1 USE TAGWRAP INSTEAD");
+
 	[SharpFunction(Name = "endtag", MinArgs = 1, MaxArgs = 1, Flags = FunctionFlags.Regular)]
-	public static ValueTask<CallState> endtag(IMUSHCodeParser parser, SharpFunctionAttribute _2)
-	{
-		throw new NotImplementedException();
-	}
+	public static ValueTask<CallState> EndTag(IMUSHCodeParser parser, SharpFunctionAttribute _2)
+		=> ValueTask.FromResult<CallState>("#-1 USE TAGWRAP INSTEAD");
+
 	[SharpFunction(Name = "tagwrap", MinArgs = 2, MaxArgs = 3, Flags = FunctionFlags.Regular)]
-	public static ValueTask<CallState> tagwrap(IMUSHCodeParser parser, SharpFunctionAttribute _2)
+	public static ValueTask<CallState> TagWrap(IMUSHCodeParser parser, SharpFunctionAttribute _2)
 	{
 		var args = parser.CurrentState.ArgumentsOrdered;
 		var tagName = args["0"].Message!.ToPlainText();
 		var content = args["1"].Message!;
-		
+
 		// Build the HTML string
 		var result = new System.Text.StringBuilder();
-		
+
 		// Build opening tag
 		result.Append('<');
 		result.Append(tagName);
-		
+
 		// Add attributes if provided
 		Microsoft.FSharp.Core.FSharpOption<string>? attributes = null;
 		if (args.Count > 2)
@@ -59,26 +55,27 @@ public partial class Functions
 				attributes = Microsoft.FSharp.Core.FSharpOption<string>.Some(attrText);
 			}
 		}
-		
+
 		result.Append('>');
-		
+
 		// Add content (preserving formatting)
 		result.Append(content.ToPlainText());
-		
+
 		// Add closing tag
 		result.Append("</");
 		result.Append(tagName);
 		result.Append('>');
-		
+
 		// Create HTML markup structure for semantic information
-		var htmlMarkup = attributes == null 
+		var htmlMarkup = attributes == null
 			? HtmlMarkup.Create(tagName)
-			: HtmlMarkup.Create(tagName, Microsoft.FSharp.Core.FSharpOption<Microsoft.FSharp.Core.FSharpOption<string>>.Some(attributes));
-		
+			: HtmlMarkup.Create(tagName,
+				Microsoft.FSharp.Core.FSharpOption<Microsoft.FSharp.Core.FSharpOption<string>>.Some(attributes));
+
 		// Return a MarkupString that contains both the HTML markup structure
 		// and the actual HTML text (so it appears in both ToString() and ToPlainText())
 		var wrappedContent = MModule.markupSingle2(htmlMarkup, content);
-		
+
 		// But for now, return the plain HTML string since tests expect it
 		return ValueTask.FromResult(new CallState(result.ToString()));
 	}
@@ -89,45 +86,45 @@ public partial class Functions
 		// wsjson() sends JSON data out-of-band (via websocket/GMCP/etc)
 		// First argument is the JSON content to send
 		// Second optional argument is the player/target (defaults to enactor)
-		
+
 		var jsonContent = parser.CurrentState.Arguments["0"].Message!.ToPlainText();
 		var executor = await parser.CurrentState.KnownExecutorObject(Mediator!);
-		
+
 		AnySharpObject target;
-		
+
 		if (parser.CurrentState.Arguments.ContainsKey("1"))
 		{
 			// Target specified - locate the player
 			var targetRef = parser.CurrentState.Arguments["1"].Message!.ToPlainText();
 			var locateResult = await LocateService!.LocateAndNotifyIfInvalid(
-				parser, 
-				executor, 
-				executor, 
-				targetRef, 
+				parser,
+				executor,
+				executor,
+				targetRef,
 				PlayersPreference | AbsoluteMatch);
-			
+
 			if (locateResult.IsError)
 			{
 				return new CallState(locateResult.AsError);
 			}
-			
+
 			target = locateResult.AsAnyObject;
 		}
 		else
 		{
 			target = executor;
 		}
-		
+
 		// TODO: Implement actual websocket/out-of-band communication
 		// For now, this is a placeholder that sends the JSON as a regular notification
 		// In a full implementation, this would:
 		// 1. Check if the target connection supports websockets/GMCP
 		// 2. Send the JSON data through the appropriate out-of-band channel
 		// 3. Return empty string (since OOB data doesn't display in-band)
-		
+
 		// Placeholder: Send as notification for now
 		// await NotifyService!.Notify(target, jsonContent, executor, INotifyService.NotificationType.Announce);
-		
+
 		// Return empty string - OOB data doesn't produce visible output
 		return CallState.Empty;
 	}
@@ -138,45 +135,45 @@ public partial class Functions
 		// wshtml() sends HTML data out-of-band (via websocket/GMCP/etc)
 		// First argument is the HTML content to send
 		// Second optional argument is the player/target (defaults to enactor)
-		
+
 		var htmlContent = parser.CurrentState.Arguments["0"].Message!.ToPlainText();
 		var executor = await parser.CurrentState.KnownExecutorObject(Mediator!);
-		
+
 		AnySharpObject target;
-		
+
 		if (parser.CurrentState.Arguments.ContainsKey("1"))
 		{
 			// Target specified - locate the player
 			var targetRef = parser.CurrentState.Arguments["1"].Message!.ToPlainText();
 			var locateResult = await LocateService!.LocateAndNotifyIfInvalid(
-				parser, 
-				executor, 
-				executor, 
-				targetRef, 
+				parser,
+				executor,
+				executor,
+				targetRef,
 				PlayersPreference | AbsoluteMatch);
-			
+
 			if (locateResult.IsError)
 			{
 				return new CallState(locateResult.AsError);
 			}
-			
+
 			target = locateResult.AsAnyObject;
 		}
 		else
 		{
 			target = executor;
 		}
-		
+
 		// TODO: Implement actual websocket/out-of-band communication
 		// For now, this is a placeholder that sends the HTML as a regular notification
 		// In a full implementation, this would:
 		// 1. Check if the target connection supports websockets/HTML
 		// 2. Send the HTML data through the appropriate out-of-band channel
 		// 3. Return empty string (since OOB data doesn't display in-band)
-		
+
 		// Placeholder: Send as notification for now
 		// await NotifyService!.Notify(target, htmlContent, executor, INotifyService.NotificationType.Announce);
-		
+
 		// Return empty string - OOB data doesn't produce visible output
 		return CallState.Empty;
 	}
