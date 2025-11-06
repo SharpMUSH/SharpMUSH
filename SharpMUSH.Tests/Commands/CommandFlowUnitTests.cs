aacd79a0396d37c55f1c3f73be35597eef1463fe
HarryCordewener <admin@twilightdays.org>
2025-11-05 18:05:13 -0600

    What the heck is this?

--- SharpMUSH.Tests/Commands/CommandFlowUnitTests.cs
+++ SharpMUSH.Tests/Commands/CommandFlowUnitTests.cs
@@ -20,16 +20,16 @@ public class CommandFlowUnitTests
 	private IMUSHCodeParser Parser => WebAppFactoryArg.CommandParser;
 
 	[Test]
-	[Arguments("@ifelse 1=@pemit #1=1 True,@pemit #2=1 False", "1 True")]
-	[Arguments("@ifelse 0=@pemit #1=2 True,@pemit #2=2 False", "2 False")]
+	[Arguments("@ifelse 1=@pemit #1=1 True,@pemit #1=1 False", "1 True")]
+	[Arguments("@ifelse 0=@pemit #1=2 True,@pemit #1=2 False", "2 False")]
 	[Arguments("@ifelse 1=@pemit #1=3 True", "3 True")]
-	[Arguments("@ifelse 1={@pemit #1=4 True},{@pemit #2=4 False}", "4 True")]
-	[Arguments("@ifelse 0={@pemit #1=5 True},{@pemit #2=5 False}", "5 False")]
+	[Arguments("@ifelse 1={@pemit #1=4 True},{@pemit #1=4 False}", "4 True")]
+	[Arguments("@ifelse 0={@pemit #1=5 True},{@pemit #1=5 False}", "5 False")]
 	[Arguments("@ifelse 1={@pemit #1=6 True}", "6 True")]
 	public async ValueTask IfElse(string str, string expected)
 	{
 		Console.WriteLine("Testing: {0}", str);
-		await Parser.CommandListParse(MModule.single(str));
+		var result = await Parser.CommandListParse(MModule.single(str));
 
 		await NotifyService.Received()
 			.Notify(Arg.Any<AnySharpObject>(), Arg.Is<OneOf<MString, string>>(msg =>
