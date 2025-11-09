f00cf425f5dd26208df06f009568318b875c5b53
HarryCordewener <admin@twilightdays.org>
2025-11-08 21:48:54 -0600

    Move wizard flag to position 0 to ensure #1 is set up right.

--- SharpMUSH.Database.ArangoDB/Migrations/Migration_CreateDatabase.cs
+++ SharpMUSH.Database.ArangoDB/Migrations/Migration_CreateDatabase.cs
@@ -922,7 +922,7 @@ public class Migration_CreateDatabase : IArangoMigration
 		var attributeFlags = await CreateInitialAttributeFlags(migrator, handle);
 		var powers = await CreateInitialPowers(migrator, handle);
 		var attributeEntries = await CreateInitialSharpAttributeEntries(migrator, handle);
-		var wizard = flags[18];
+		var wizard = flags[0];
 
 		await migrator.Context.Document.CreateAsync(handle, DatabaseConstants.IsObject, new SharpEdge { From = roomTwoRoom.Id, To = roomTwoObj.Id });
 		await migrator.Context.Document.CreateAsync(handle, DatabaseConstants.IsObject, new SharpEdge { From = roomZeroRoom.Id, To = roomZeroObj.Id });
@@ -938,6 +938,18 @@ public class Migration_CreateDatabase : IArangoMigration
 	private static async Task<List<ArangoUpdateResult<ArangoVoid>>> CreateInitialSharpAttributeEntries(
 		IArangoMigrator migrator, ArangoHandle handle) =>
 	[
+		await migrator.Context.Document.CreateAsync(handle, DatabaseConstants.ObjectFlags, new
+		{
+			Name = "WIZARD",
+			Symbol = "W",
+			System = true,
+			TypeRestrictions = DatabaseConstants.typesAll,
+			SetPermissions = DatabaseConstants.permissionsTrusted
+				.Union(DatabaseConstants.permissionsWizard)
+				.Union(DatabaseConstants.permissionsLog),
+			UnsetPermissions = DatabaseConstants.permissionsTrusted
+				.Union(DatabaseConstants.permissionsWizard),
+		}),
 				await migrator.Context.Document.CreateAsync(handle, DatabaseConstants.AttributeEntries,
 			new
 			{
@@ -2899,18 +2911,6 @@ public class Migration_CreateDatabase : IArangoMigration
 			Symbol = "V",
 			System = true,
 			TypeRestrictions = DatabaseConstants.typesAll
-		}),
-		await migrator.Context.Document.CreateAsync(handle, DatabaseConstants.ObjectFlags, new
-		{
-			Name = "WIZARD",
-			Symbol = "W",
-			System = true,
-			TypeRestrictions = DatabaseConstants.typesAll,
-			SetPermissions = DatabaseConstants.permissionsTrusted
-				.Union(DatabaseConstants.permissionsWizard)
-				.Union(DatabaseConstants.permissionsLog),
-			UnsetPermissions = DatabaseConstants.permissionsTrusted
-				.Union(DatabaseConstants.permissionsWizard),
 		}),
 		await migrator.Context.Document.CreateAsync(handle, DatabaseConstants.ObjectFlags, new
 		{
