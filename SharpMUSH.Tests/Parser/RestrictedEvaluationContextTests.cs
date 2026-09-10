06747d08b0b155f1af6b4c964e8e69e30a23a255
Harry Cordewener <harry@cordewener.com>
2026-09-09 23:32:59 -0500

    Integrate reviewed restricted expressions into main

---
+++ SharpMUSH.Tests/Parser/RestrictedEvaluationContextTests.cs
@@ -0,0 +1,51 @@
+using SharpMUSH.Library.ParserInterfaces;
+
+namespace SharpMUSH.Tests.Parser;
+
+public class RestrictedEvaluationContextTests
+{
+	[Test]
+	public async Task NestedContextsIntersectAndRestoreAfterExceptions()
+	{
+		var outer = new EvaluationRestrictions(["add"]);
+		using (outer.Enter())
+		{
+			try
+			{
+				using var inner = new EvaluationRestrictions(["add", "sub"]).Enter();
+				await Assert.That(EvaluationRestrictions.Current!.Allows("add")).IsTrue();
+				await Assert.That(EvaluationRestrictions.Current.Allows("sub")).IsFalse();
+				await Assert.That(EvaluationRestrictions.Current.AllowObjectDataAccess).IsFalse();
+				throw new InvalidOperationException("test unwind");
+			}
+			catch (InvalidOperationException ex) { await Assert.That(ex.Message).IsEqualTo("test unwind"); }
+			await Assert.That(EvaluationRestrictions.Current).IsEqualTo(outer);
+		}
+		await Assert.That(EvaluationRestrictions.Current).IsNull();
+	}
+
+	[Test]
+	public async Task ConcurrentContextsNeverShareAllowlists()
+	{
+		var entered = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+		var finish = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+		var first = Task.Run(async () =>
+		{
+			using var scope = new EvaluationRestrictions(["add"]).Enter();
+			entered.SetResult();
+			await finish.Task;
+			await Assert.That(EvaluationRestrictions.Current!.Allows("add")).IsTrue();
+			await Assert.That(EvaluationRestrictions.Current.Allows("sub")).IsFalse();
+		});
+		await entered.Task;
+		try
+		{
+			using var scope = new EvaluationRestrictions(["sub"]).Enter();
+			await Assert.That(EvaluationRestrictions.Current!.Allows("sub")).IsTrue();
+			await Assert.That(EvaluationRestrictions.Current.Allows("add")).IsFalse();
+		}
+		finally { finish.SetResult(); }
+		await first;
+		await Assert.That(EvaluationRestrictions.Current).IsNull();
+	}
+}
